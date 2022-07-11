51f364518f72e472c6fe72b2c8714c50b799ac2d
Aaron Feickert <66188213+AaronFeickert@users.noreply.github.com>
2022-07-11 13:31:08 -0500

    Add effective entropy functionality

--- lib/generate_password.dart
+++ lib/generate_password.dart
@@ -4,29 +4,51 @@
 /// Our character set is a subset of `[0-9a-zA-Z]` where we exclude similar-looking symbols.
 /// To help the user, we exclude the following set: `{o, O, 0, l, I, 1}`
 /// This means our character set contains (10 + 26 + 26 - 6) = 56 symbols.
-/// By using a 16-character password, we achive `log2(56^16) ~ 93` bits, which should be plenty.
 /// 
-/// This is the default length, but you can override it.
+/// By default, we play it super safe and generate a 128-bit password, which assumes no subsequent PBKDF is used.
+/// This is the default behavior, but you can specify your own password length.
+/// You can also use the `lengthFromEntropy` function to compute the length required for a target entropy level, and pass this length instead.
+/// This might be useful if you know the password is passed through a suitable PBKDF and can tolerate lower effective entropy.
+/// Your call.
 
 import 'dart:math';
 
 const String _safeLowerCase = 'abcdefghijkmnpqrstuvwxyz'; // all letters except `l, o`
 const String _safeUpperCase = 'ABCDEFGHJKLMNPQRSTUVWXYZ'; // all letters except 'I, O'
 const String _safeDigits = '23456789'; // all digits except `0, 1`
-const int _passwordLength = 16; // about 93 bits of effective entropy using this character set
+const int _entropyTarget = 128; // this assumes no subsequent PBKDF
 
-// Returns the default length for testing purposes
-int getPasswordLength() {
-  return _passwordLength;
+/// Return the default length for testing purposes
+int getDefaultLength() {
+  return lengthFromEntropy(_entropyTarget);
 }
 
-// Returns the full character set for testing purposes
+/// Return the full character set for testing purposes
 String getCharacterSet() {
   // Set up the complete character set
   return _safeLowerCase + _safeUpperCase + _safeDigits;
 }
 
-String generatePassword({int length = _passwordLength}) {
+/// Estimate the effective entropy (in bits) associated to a random password of a given length
+/// This will round down to be safe; there is a loss of precision associated with this construction anyway
+/// NOTE: This is only valid for a random password!
+int entropyFromLength(int length) {
+  return length*log(getCharacterSet().length) ~/ log(2); // log2([size of character set]^[length of password])
+}
+
+/// Estimate the password length required to meet an entropy target (in bits)
+/// This will round up to be safe; there is a loss of precision associated with this construction anyway
+/// NOTE: This is only valid for a random password!
+int lengthFromEntropy(int entropy) {
+  return (entropy*log(2) ~/ log(getCharacterSet().length)) + 1;
+}
+
+/// Generate a random password of a given length using the hardcoded character set
+/// If you leave out the length, a password of 128-bit effective entropy is generated
+String generatePassword([int? length]) {
+  // Use the default if required
+  length ??= getDefaultLength();
+
   // Require a cryptographically-secure RNG
   var rng = Random.secure();
 
--- test/generate_password_test.dart
+++ test/generate_password_test.dart
@@ -6,7 +6,7 @@ void main() {
   test('character set is valid', () {
     final String characters = getCharacterSet();
 
-    // Correct number of symbols
+    // The character set has the length we expected when writing it
     expect(characters.length, 56);
 
     // No duplicates
@@ -23,15 +23,23 @@ void main() {
 
   // Default password length
   test('default length', () {
-    expect(generatePassword().length, getPasswordLength());
+    // With our character set, a 128-bit effective entropy target should generate a 23-character password
+    String password = generatePassword();
+    expect(password.length, getDefaultLength());
+    expect(getDefaultLength(), 23);
   });
 
   // Other password lengths
   List<int> lengths = [0, 1, 2, 32, 64, 128];
   for (int length in lengths) {
     test('length $length', () {
-      expect(generatePassword(length: length).length, length);
+      expect(generatePassword(length).length, length);
     });
   }
-  
+
+  // Entropy from length
+  test('entropy from length', () {
+    // With our character set, a 16-character password has ~92 bits of effective entropy
+    expect(entropyFromLength(16), 92);
+  });
 }
